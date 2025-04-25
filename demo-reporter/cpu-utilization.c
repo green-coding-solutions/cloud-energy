7c2eb2dc3671c91cca38c872efc3375f224db53a
Arne Tarara <arne@datafuse.de>
2025-04-25 09:37:53 +0200

    Reworked CPU Utilization provider to also allow for time value to be printed; Removed whole user_hz as we only need ratios

--- demo-reporter/cpu-utilization.c
+++ demo-reporter/cpu-utilization.c
@@ -9,13 +9,14 @@ typedef struct procfs_time_t { // struct is a specification and this static make
     unsigned long user_time;
     unsigned long nice_time;
     unsigned long system_time;
-    unsigned long idle_time;
+    unsigned long wait_time;
     unsigned long iowait_time;
     unsigned long irq_time;
     unsigned long softirq_time;
     unsigned long steal_time;
     unsigned long guest_time;
     unsigned long compute_time; // custom attr by us not in standard /proc/stat format
+    unsigned long idle_time; // custom attr by us not in standard /proc/stat format
 } procfs_time_t;
 
 
@@ -37,7 +38,7 @@ static void read_cpu_proc(procfs_time_t* procfs_time_struct) {
         exit(1);
     }
 
-    fscanf(fd, "cpu %ld %ld %ld %ld %ld %ld %ld %ld %ld", &procfs_time_struct->user_time, &procfs_time_struct->nice_time, &procfs_time_struct->system_time, &procfs_time_struct->idle_time, &procfs_time_struct->iowait_time, &procfs_time_struct->irq_time, &procfs_time_struct->softirq_time, &procfs_time_struct->steal_time, &procfs_time_struct->guest_time);
+    fscanf(fd, "cpu %ld %ld %ld %ld %ld %ld %ld %ld %ld", &procfs_time_struct->user_time, &procfs_time_struct->nice_time, &procfs_time_struct->system_time, &procfs_time_struct->wait_time, &procfs_time_struct->iowait_time, &procfs_time_struct->irq_time, &procfs_time_struct->softirq_time, &procfs_time_struct->steal_time, &procfs_time_struct->guest_time);
 
     // debug
     // printf("Read: cpu %ld %ld %ld %ld %ld %ld %ld %ld %ld\n", procfs_time_struct->user_time, procfs_time_struct->nice_time, procfs_time_struct->system_time, procfs_time_struct->idle_time, procfs_time_struct->iowait_time, procfs_time_struct->irq_time, procfs_time_struct->softirq_time, procfs_time_struct->steal_time, procfs_time_struct->guest_time);
@@ -47,65 +48,61 @@ static void read_cpu_proc(procfs_time_t* procfs_time_struct) {
     // after this multiplication we are on microseconds
     // integer division is deliberately, cause we don't loose precision as *1000000 is done before
 
-    procfs_time_struct->user_time = (procfs_time_struct->user_time*1000000)/user_hz;
-    procfs_time_struct->nice_time = (procfs_time_struct->nice_time*1000000)/user_hz;
-    procfs_time_struct->system_time = (procfs_time_struct->system_time*1000000)/user_hz;
-    procfs_time_struct->idle_time = (procfs_time_struct->idle_time*1000000)/user_hz;
-    procfs_time_struct->iowait_time = (procfs_time_struct->iowait_time*1000000)/user_hz;
-    procfs_time_struct->irq_time = (procfs_time_struct->irq_time*1000000)/user_hz;
-    procfs_time_struct->softirq_time = (procfs_time_struct->softirq_time*1000000)/user_hz;
-    procfs_time_struct->steal_time = (procfs_time_struct->steal_time*1000000)/user_hz;
-    procfs_time_struct->guest_time = (procfs_time_struct->guest_time*1000000)/user_hz;
-    procfs_time_struct->compute_time = procfs_time_struct->user_time + procfs_time_struct->nice_time + procfs_time_struct->system_time + procfs_time_struct->iowait_time + procfs_time_struct->irq_time + procfs_time_struct->softirq_time + procfs_time_struct->steal_time + procfs_time_struct->guest_time;
+    procfs_time_struct->idle_time = procfs_time_struct->wait_time + procfs_time_struct->iowait_time + procfs_time_struct->irq_time + procfs_time_struct->softirq_time;
+    // in /proc/stat nice time is NOT included in the user time! (it is in cgroups however though)
+    procfs_time_struct->compute_time = procfs_time_struct->user_time + procfs_time_struct->system_time + procfs_time_struct->nice_time;
+
 }
 
 
-static int output_stats() {
-
-    long int  idle_reading, compute_time_reading;
+static int output_stats(int show_diff_time) {
+    long int idle_reading, compute_time_reading;
     procfs_time_t main_cpu_reading_before;
     procfs_time_t main_cpu_reading_after;
-    struct timeval now;
+    struct timeval start, end;
 
-    gettimeofday(&now, NULL); // will set now
-    read_cpu_proc(&main_cpu_reading_before); // will set main_cpu_reading_before
+    gettimeofday(&start, NULL);
+    read_cpu_proc(&main_cpu_reading_before);
 
-    usleep(msleep_time*1000);
+    usleep(msleep_time * 1000);
 
-    read_cpu_proc(&main_cpu_reading_after); // will set main_cpu_reading_before
+    gettimeofday(&end, NULL);
+    read_cpu_proc(&main_cpu_reading_after);
+
+    // Calculate actual sleep duration in seconds (as double)
+    double slept_time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1.0e6;
 
     idle_reading = main_cpu_reading_after.idle_time - main_cpu_reading_before.idle_time;
     compute_time_reading = main_cpu_reading_after.compute_time - main_cpu_reading_before.compute_time;
 
-    // debug
-    // printf("Main CPU Idle Reading: %ld\nMain CPU Compute Time Reading: %ld\n", idle_reading, compute_time_reading);
-    // printf("%ld%06ld %f\n", now.tv_sec, now.tv_usec, (double)compute_time_reading / (double)(compute_time_reading+idle_reading));
+    double output = 100.0 * (double)compute_time_reading / (compute_time_reading + idle_reading);
+    if (output < 0.0) output = 0.0;
+    if (output > 100.0) output = 100.0;
 
-    // main output to Stdout
-    double output = 100*((double)compute_time_reading) / ((double)(compute_time_reading+idle_reading));
-    if(output < 0.0) {
-        output = 0.0;
-    } else if(output > 100.0) {
-        output = 100.0;
+    if (show_diff_time == 1) {
+        printf("%.6f %.2f\n", slept_time, output);  // Print sleep time and utilization
+    } else {
+        printf("%.2f\n", output);  // Print  utilization only
     }
-    printf("%.2f\n", output); // Deliberate integer conversion. Precision with 0.01% is good enough
-
     return 1;
 }
+
 
 int main(int argc, char **argv) {
 
     int c;
+    int show_diff_time = 0;
 
     setvbuf(stdout, NULL, _IONBF, 0);
     user_hz = sysconf(_SC_CLK_TCK);
 
-    while ((c = getopt (argc, argv, "i:h")) != -1) {
+    while ((c = getopt (argc, argv, "i:hx")) != -1) {
         switch (c) {
         case 'h':
             printf("Usage: %s [-i msleep_time] [-h]\n\n",argv[0]);
             printf("\t-h      : displays this help\n");
             printf("\t-i      : specifies the milliseconds sleep time that will be slept between measurements\n\n");
+            printf("\t-x      : show diff time before utilization\n\n");
 
             struct timespec res;
             double resolution;
@@ -120,6 +117,9 @@ int main(int argc, char **argv) {
         case 'i':
             msleep_time = atoi(optarg);
             break;
+        case 'x':
+            show_diff_time = 1;
+            break;
         default:
             fprintf(stderr,"Unknown option %c\n",c);
             exit(-1);
@@ -127,7 +127,7 @@ int main(int argc, char **argv) {
     }
 
     while(1) {
-        output_stats();
+        output_stats(show_diff_time);
     }
 
     return 0;
